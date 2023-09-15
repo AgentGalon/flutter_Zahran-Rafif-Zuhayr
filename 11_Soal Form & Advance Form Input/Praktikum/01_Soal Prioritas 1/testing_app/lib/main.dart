1200043378d485b1b30d5217adae450554644126
AgentGalon <zahranrafif911@gmail.com>
2023-09-16 03:24:11 +0700

    add task, gif and summary

---
+++ 11_Soal Form & Advance Form Input/Praktikum/01_Soal Prioritas 1/testing_app/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'android/material_app.dart';
+// import 'ios/cupertino_app.dart';
+// import 'Soal Eksplorasi/material_app.dart';
+
+void main() {
+  runApp(const App());
+}
